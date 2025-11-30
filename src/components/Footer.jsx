a34bd78aff10742753259a08e53a559dc4c58f2f
Jennifer <jenniferromanmuerte@gmail.com>
2025-11-30 12:33:38 +0100

    Added style in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,35 +1,27 @@
 import { NavLink } from "react-router";
+import "../styles/Footer.scss";
 
-function Footer () {
+function Footer() {
   return (
     <footer className="footer">
-      <nav>
-        <ul>
-          <li className
-          ="footer__menu-item">
-            <NavLink className
-            ="footer__menu-link" to="#/">
-              A jugar
-            </NavLink>
-          </li>
-          <li className
-          ="footer__menu-item">
-            <NavLink className
-            ="footer__menu-link active" to="/Instructions">
-              ¿Cómo se juega?
-            </NavLink>
-          </li>
-          <li className
-          ="footer__menu-item">
-            <NavLink className
-            ="footer__menu-link" to="#/options">
-              Más opciones
-            </NavLink>
-          </li>
-        </ul>
+      <nav className="footer__menu">
+        <li className="footer__menu-item">
+          <NavLink className="footer__menu-link" to="#/">
+            A jugar
+          </NavLink>
+        </li>
+        <li className="footer__menu-item">
+          <NavLink className="footer__menu-link" to="/Instructions">
+            ¿Cómo se juega?
+          </NavLink>
+        </li>
+        <li className="footer__menu-item">
+          <NavLink className="footer__menu-link" to="#/options">
+            Más opciones
+          </NavLink>
+        </li>
       </nav>
-      <small className
-      ="footer__copy">© Adalab</small>
+      <small className="footer__copy">© Adalab</small>
     </footer>
   );
 }
